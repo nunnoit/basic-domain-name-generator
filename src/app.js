4716756c5d9bb9608b37aa2e01ab4618a99542fc
nunnoit <devit@tuta.io>
2022-09-17 02:42:21 +0000

    push code

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let opt1 = ["web", "cool", "poke", "pika"];
+  let opt2 = ["wii", "free", "2", "dev"];
+  let extencions = [".com", ".net", ".org"];
+  let textoH1 = document.querySelector("#domaingen");
+
+  let aux = "";
+  for (let i = 0; i < opt1.length; i++) {
+    for (let j = 0; j < extencions.length; j++) {
+      console.log(opt1[i] + " " + extencions[j]);
+      aux +=
+        "<p>" +
+        opt2[i] +
+        opt1[i] +
+        "" +
+        extencions[j] +
+        "<br><br>" +
+        opt1[i] +
+        opt2[i] +
+        "" +
+        extencions[j] +
+        "</p> ";
+    }
+  }
+  textoH1.innerHTML = aux;
 };
